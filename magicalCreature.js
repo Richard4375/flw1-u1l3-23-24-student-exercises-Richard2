fc59aa5f8ad3d69b8840644c6cee183d18748593
Richard4375 <140004673+Richard4375@users.noreply.github.com>
2023-10-24 22:31:57 +0000

    completed tasks

--- magicalCreature.js
+++ magicalCreature.js
@@ -1,36 +1,46 @@
 // Welcome to the magical zoo! Your task is to create and manage a list of magical creatures. Let's have some fun!
 
 // 1. Declare a variable named 'creatureName' and assign it a magical creature's name as a string.
+let creatureName = 'dragon'
 
 
 
 // 2. Declare a variable named 'creatureDescription' and describe its magical properties as a string.
-
+let creatureDescription = "cool and big"
 
 // 3. Do you believe that this creature is friendly to humans? 
 // Declare a variable named 'isFriendly' and assign it a boolean value.
-
+let isFriendly = false
 
 
 // 4. The magical zoo has an age restriction! Declare a variable named 'visitorAge' and assign it a random age between 1 and 60.
 // Hint: Use Math.floor() to round a number up.
 // Hint: Use Math.random() for generating random numbers.
-
+const visitorAge = Math.floor(Math.random() * 60) + 1
+console.log(visitorAge)
 
 
 // 5. Determine if the visitor is allowed to see the creature.
 // Declare a variable named 'canVisit' and assign it an expression to see 
 // if the visitorAge variable is greater than 15.
 // Hint: use this resource if you need support: https://www.w3schools.com/js/js_booleans.asp 
+if(visitorAge > 15){
+  let canVisit = true
+  console.log(canVisit)
+}else{
+  let canVisit = false
+  console.log(canVisit)
+}
 
+  
 
 
 // 6. Combine all the information into a summary.
 // Declare a variable named 'zooSummary' and combine all the above information using concatenation.
-
+let zooSummary = (creatureName + " is " + creatureDescription + " and you need to be 15 years old to visit it.")
 
 // 7. Print the summary using console.log()
-
+console.log(zooSummary)
 
 
 // Challenge:
@@ -38,3 +48,34 @@
 //    Repeat Tasks 1-6 for each creature, then combine all summaries into a single variable 'allZooSummaries'.
 // 9. Print the 'allZooSummaries' to see the details of all the creatures in the magical zoo.
 
+//creature 2 
+let creature2 = 'unicorn'
+let creature2Description = 'cool and magical'
+let creature2Friendly = true
+const creature2Age = Math.floor(Math.random() * 60) + 1
+console.log(creature2Age)
+if(creature2Age > 15){
+  let canVisit2 = true
+  console.log(canVisit2)
+}else{
+  let canVisit2 = false
+  console.log(canVisit2)
+}
+let creature2ZooSummary = (creature2 + " is " + creature2Description + " and you need to be 15 years old to visit it.")
+console.log(creature2ZooSummary)
+
+//creature3
+let creature3 = 'fairy'
+let creature3Description = 'smol n magical'
+let creature3Friendly = true
+const creature3Age = Math.floor(Math.random() * 60) + 1
+console.log(creature3Age)
+if(creature3Age > 15){
+  let canVisit3 = true
+  console.log(canVisit3)
+}else{
+  let canVisit3 = false
+  console.log(canVisit3)
+}
+let creature3ZooSummary = (creature3 + " is " + creature3Description + " and you need to be 15 years old to visit it.")
+console.log(creature3ZooSummary)
--- operators.js
+++ operators.js
@@ -3,26 +3,35 @@
 
 // 1. Addition
 // Create two variables, num1 and num2, and assign them any two numbers of your choice.
+let num1 = 4
+let num2 = 10
 // Add the two numbers and store the result in a new variable called sum.
 // Print the result using console.log().
+sum = num1 + num2
+console.log(sum)
 
 
 
 // 2. Subtraction
 // Subtract num2 from num1 and store the result in a new variable called difference.
 // Print the result.
-
+let difference = num1 - num2
+console.log(difference)
 
 // 3. Multiplication
 // Multiply num1 by num2 and store the result in a new variable called product.
 // Print the result.
-
+let product = num1 * num2
 
 
 // String Operations
 
 // 4. Concatenation with +
 // Create two string variables, str1 and str2, and assign them any two strings of your choice.
+let str1 = "Hello"
+let str2 = "World"
 // Concatenate (combine) the two strings using the + operator and store the result in a new variable called combinedString.
 // Print the result.
+let combinedString = str1 + str2
+console.log(combinedString)
 
